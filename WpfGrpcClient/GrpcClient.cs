60f79fdfb6b408f0330914b36a4979954e965d76
Ahmed Awadallah <aawadall@ualberta.ca>
2019-11-23 10:28:48 -0500

    new WPF client

---
+++ WpfGrpcClient/GrpcClient.cs
@@ -0,0 +1,101 @@
+﻿using Grpc.Core;
+using System;
+using System.IO;
+using static Messages.EmployeeService;
+
+namespace WpfGrpcClient
+{
+    class GrpcClient
+    {
+        public bool SslReady { get; private set; }
+        public bool Connected { get; private set; }
+        #region Constructors
+        public GrpcClient()
+        {
+            Environment.SetEnvironmentVariable("GRPC_DNS_RESOLVER", "native");
+            SslReady = SetupSsl(caCertLocation: @"certs/ca.crt",
+                certLocation: @"certs/client.crt",
+                clientKeyLocation: @"certs/client.key");
+        }
+        #endregion
+
+        #region Helper methods
+        internal bool Connect()
+        {
+            var port = 9000;
+            Connected = Connect("DESKTOP-AQ2TIF3", port, _credentials);
+            return Connected;
+        }
+
+        private bool Connect(string hostUrl, int port, SslCredentials credentials)
+        {
+            _channel = new Channel(hostUrl, port, credentials);
+            _client = new EmployeeServiceClient(_channel);
+            try
+            {
+                Metadata metadata = new Metadata
+                {
+                    { "purpose", "ping" }
+                };
+
+                int badgeNumber = 1;
+                var result =  _client.GetByBadgeNumberAsync(new Messages.GetByBadgeNumberRequest() { BadgeNumber = badgeNumber }, metadata);
+                
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        private bool SetupSsl(string caCertLocation, string certLocation, string clientKeyLocation)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(caCertLocation))
+                {
+                    throw new ArgumentException("message", nameof(caCertLocation));
+                }
+
+                if (string.IsNullOrEmpty(certLocation))
+                {
+                    throw new ArgumentException("message", nameof(certLocation));
+                }
+
+                if (string.IsNullOrEmpty(clientKeyLocation))
+                {
+                    throw new ArgumentException("message", nameof(clientKeyLocation));
+                }
+
+                _caCert = File.ReadAllText(caCertLocation);
+                _cert = File.ReadAllText(certLocation);
+                _key = File.ReadAllText(clientKeyLocation);
+
+                _keyPair = new KeyCertificatePair(_cert, _key);
+                _credentials = new SslCredentials(_caCert, _keyPair);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            
+        }
+
+     
+        #endregion
+        #region Private fields
+        string _caCert;
+        string _cert;
+        string _key;
+        KeyCertificatePair _keyPair;
+        SslCredentials _credentials;
+        private ChannelBase _channel;
+        EmployeeServiceClient _client;
+        #endregion
+    }
+}
---
+++ WpfGrpcClient/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WpfGrpcClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        GrpcClient Client { get; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Client = new GrpcClient();
+            SslStatus.Content = Client.SslReady ? "SSL Success" : "SSL Failed";
+            SslStatus.Foreground = Client.SslReady ? Brushes.Green : Brushes.Red;
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Client.Connect();
+            ConnectionStatus.Content = Client.Connected ? "Connected" : "Disconnected";
+            ConnectionStatus.Foreground = Client.Connected ? Brushes.Green : Brushes.Red;
+        }
+    }
+}
